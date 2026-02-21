253dfef01bb6b463dd89ea5d81429bb6f831002a
Qin Geng <gengqin50@gmail.com>
2026-02-21 18:02:32 +0000

    add search handler

---
+++ constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	USER_INDEX = "user"
+	POST_INDEX = "post"
+	ES_URL = "http://35.199.176.15:9200/"
+	ES_USERNAME = "qin_username"
+	ES_PASSWORD = "qin_password"
+)
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"mysocialai/backend"
+	"mysocialai/handler"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("started-service")
+
+	backend.InitElasticsearchBackend()
+
+	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter()))
 }
